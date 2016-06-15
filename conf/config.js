7c7e325f25102a498043fa3719e467a1f41ae484
Victor Gil <vgil@switch.com.uy>
2016-06-15 05:25:16 -0300

    added mongolab support

--- conf/config.js
+++ conf/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 const debug = require('debug')('comment-system:config');
-mongoose.connect('mongodb://127.0.0.1/comment-system');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1/comment-system');
 mongoose.connection.on('open', function(){
   debug('Successfully connected to MongoDB');
 })
